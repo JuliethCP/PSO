0b51e58a94b166e92058ef9f35492f8735ce7a83
Julieth Campos <juliethcp22@gmail.com>
2023-10-26 20:56:20 -0600

    a

--- src/helpers/ai-sdk/loader.js
+++ src/helpers/ai-sdk/loader.js
@@ -22,7 +22,7 @@ async function initAiSdk() {
 
   aiSdkInstance = await globalThis.CY.loader()
     // TODO INSERT YOUR LICENSE KEY HERE
-    .licenseKey("insert-here-your-license-key") // <--- ##############
+    .licenseKey("3afd07bfe6e9b3aed20a3ea33a4656b6cc83a134bedc") // <--- ##############
     .source(source)
     .addModule(globalThis.CY.modules().FACE_DETECTOR.name)
     .addModule(globalThis.CY.modules().FACE_EMOTION.name, {
